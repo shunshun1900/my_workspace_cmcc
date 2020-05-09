cf6ae5017fc4da85c6de6f6fa438c9327a55fc49
shunshun <shun_shun@139.com>
2020-05-09 17:59:51 +0800

    add user basic

---
+++ dim_table.sql
@@ -0,0 +1,4 @@
+-- msc ID
+data hdpyp02.dim_msc_id;
+set dim_msc_id;
+run;
---
+++ user_basic.sql
@@ -0,0 +1,29 @@
+-- personal user month
+proc sql;
+create table tmp_1 as
+select phone_no,user_id,imsi 
+from share_yy.kb_&month
+where  USER_STATE_ID in (100,111)  and lvl1_plan_id in (11010,11020,11030)
+and  m2m_flag^=1 and data_crd_flag^=1 and length(phone_no)=11;
+quit;
+
+-- personal user day
+proc sql;
+create table tmp_user_info_&cur_day as
+select 
+phone_no,
+user_id,
+join_date, 
+online_id,
+DATA_CRD_FLAG,
+WAP20_FLAG,
+ USER_STATE_ID
+from dw61.dwa_usr_info_&cur_day
+where  USER_STATE_ID in (100,111)  and lvl1_plan_id in (11010,11020,11030) and length(phone_no)=11;
+quit;
+
+-- pay user month
+proc sql;
+select * from share_yy.kb_&month
+where is_dykz=1 and m2m_flag^=1 and data_crd_flag^=1;
+quit;
